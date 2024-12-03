d7aeaee15c25c9964caa2716a2b34538ed40f3d8
rafaelsety <mhmmdrafael28@gmail.com>
2024-12-03 17:45:20 +0700

    deployment-1

--- main.py
+++ main.py
@@ -275,4 +275,4 @@ def DataRecap():
     
 if __name__ == '__main__': 
     port = int(os.getenv('PORT', 8080))
-    app.run(host ='0.0.0.0', port=port, debug=True)
+    app.run(host ='0.0.0.0', port=port)
